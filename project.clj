766b5c3f41cf7923139adae3664c663d30410ff2
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-22 16:49:24 +0100

    Add DB finder

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject tranchis/photon-config "0.9.4"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [io.github.lukehutch/fast-classpath-scanner "1.9.7"]])
---
+++ test/photon_config/core_test.clj
@@ -0,0 +1,7 @@
+(ns photon-config.core-test
+  (:require [clojure.test :refer :all]
+            [photon-config.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
