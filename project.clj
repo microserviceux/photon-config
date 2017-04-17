e27fe9d5a0208b6a24ccc0b04afd1630667a0c58
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-04-17 18:16:40 +0200

    Update group to muoncore

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tranchis/photon-config "0.9.7"
+(defproject muoncore/photon-config "0.9.7"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :repositories [["snapshots"
